eb70abb4b27bc7e2b0aec57d48751c7280069f84
Nathan Smith <nsmith_piano@hotmail.com>
2019-09-04 20:54:17 -0500

    replace UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,223 +1,60 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  CssBaseline,
-  Drawer,
-  List,
-  Divider,
-  ListItem,
-  ListItemIcon,
-  ListItemText
-} from "@material-ui/core";
-
-import {
-  Menu as MenuIcon,
-  ChevronLeft as ChevronLeftIcon,
-  Dashboard as DashboardIcon,
-  EventNote as EventNoteIcon,
-  People as PeopleIcon
-} from "@material-ui/icons";
-
-import UserList from "./UserList";
-import classNames from "classnames";
-
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    display: "flex"
-  },
-  toolbar: {
-    paddingRight: 24
-  },
-  toolbarIcon: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    backgroundColor: "#383838"
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36
-  },
-  menuButtonHidden: {
-    display: "none"
-  },
-  title: {
-    flexGrow: 1
-  },
-  drawerPaper: {
-    position: "relative",
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerPaperClose: {
-    overflowX: "hidden",
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    width: theme.spacing.unit * 7,
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing.unit * 9
-    }
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    height: "100vh",
-    overflow: "auto"
-  }
-});
+import React, { Component } from 'react'
+import './App.css';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import BipartiteGraph from './BipartiteGraph'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedView: "Home",
-      open: true
-    };
-  }
-
-  setSelectedView(viewName) {
-    this.setState({
-      selectedView: viewName
-    });
-  }
-
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
-
   render() {
-    const { classes } = this.props;
-
     return (
-      <React.Fragment>
-        <CssBaseline />
-        <div className={classes.root}>
-          <AppBar
-            position="absolute"
-            className={classNames(
-              classes.appBar,
-              this.state.open && classes.appBarShift
-            )}
-          >
-            <Toolbar
-              disableGutters={!this.state.open}
-              className={classes.toolbar}
-            >
-              <IconButton
-                color="inherit"
-                aria-label="Open drawer"
-                onClick={this.handleDrawerOpen}
-                className={classNames(
-                  classes.menuButton,
-                  this.state.open && classes.menuButtonHidden
-                )}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography
-                component="h1"
-                variant="title"
-                color="inherit"
-                noWrap
-                className={classes.title}
-              >
-                Welcome To GRANDstack
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            variant="permanent"
-            classes={{
-              paper: classNames(
-                classes.drawerPaper,
-                !this.state.open && classes.drawerPaperClose
-              )
-            }}
-            open={this.state.open}
-          >
-            <div className={classes.toolbarIcon}>
-              <IconButton onClick={this.handleDrawerClose}>
-                <ChevronLeftIcon />
-              </IconButton>
-            </div>
-            <Divider />
-            <List>
-              <div>
-                <ListItem button onClick={() => this.setSelectedView("Home")}>
-                  <ListItemIcon>
-                    <DashboardIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Home" />
-                </ListItem>
+    <Query
+    query={gql`
+      {
+          Person {
+            name
+            interests
+              { 
+                name
+              }
+          }
+        }  
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error</p>;
 
-                <ListItem
-                  button
-                  onClick={() => this.setSelectedView("Businesses")}
-                >
-                  <ListItemIcon>
-                    <EventNoteIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Businesses" />
-                </ListItem>
+      var people = data.Person.map(obj=> ({'name':obj.name, 'nodeLabel':'Person'}))
+      var topics = data.Person.map(obj=>obj.interests.map(obj2 => obj2.name)).flat()
+      var unique_topics = [...new Set(topics)].map(topic => ({'name':topic, 'nodeLabel':'Topic'}))
+      var node_data = people.concat(unique_topics)
+      
+      var links_data = []
+      for (var person in data.Person)
+        {var person_data = data.Person[person];
+           for (var interest in person_data.interests){
+             links_data.push({'source':person_data.name, 'target':person_data.interests[interest].name})
+           }
+           }
+      var final_data = {nodes:node_data, links: links_data}
 
-                <ListItem button onClick={() => this.setSelectedView("Users")}>
-                  <ListItemIcon>
-                    <PeopleIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Users" />
-                </ListItem>
-              </div>
-            </List>
-          </Drawer>
-          <main className={classes.content}>
-            <div className={classes.appBarSpacer} />
-
-            {/* FIXME: Use proper routing here instead  */}
-            <Typography component="div" className={classes.chartContainer}>
-              {this.state.selectedView === "Home" ? <UserList /> : null}
-              {this.state.selectedView === "Businesses" ? <div /> : null}
-              {this.state.selectedView === "Users" ? <UserList /> : null}
-            </Typography>
-          </main>
+      return (
+    
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Common Interests</h2>
         </div>
-      </React.Fragment>
-    );
+        <div>
+          <BipartiteGraph data={final_data} size={[800,500]} orientation={"horizontal"} />
+        </div>
+      </div>
+    )}}
+    </Query>    
+    )
   }
+
 }
 
-export default withStyles(styles)(App);
+
+
+export default App;
