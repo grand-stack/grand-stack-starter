260229c9286a4739976bf07c525245d0b0d24b14
Nathan Smith <nsmith_piano@hotmail.com>
2019-09-07 15:28:05 -0500

    change id to name

--- ui/src/ForceGraph2.js
+++ ui/src/ForceGraph2.js
@@ -20,11 +20,11 @@ class ForceGraph extends Component {
 
     createForceGraph() {
         const node = this.node
-        const links = props.data.links.map(d => Object.create(d));
+        const links = this.props.data.links.map(d => Object.create(d));
         const nodes = props.data.nodes.map(d => Object.create(d));
         const displaySize = this.props.size;
         const simulation = forceSimulation(nodes)
-        .force("link", forceLink(links).id(d => d.id))
+        .force("link", forceLink(links).id(d => d.name))
         .force("charge", forceManyBody())
         .force("center", forceCenter(displaySize[0] / 2, displaySize[1] / 2));
   
@@ -49,7 +49,7 @@ class ForceGraph extends Component {
         .call(drag(simulation));
   
     circle.append("title")
-        .text(d => d.id);
+        .text(d => d.name);
   
     simulation.on("tick", () => {
       link
