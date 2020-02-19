5fbceb2a98f73ea3dadc03d378464ec546ace70a
Peter Gan <S2k.Peter.Gan@live.com>
2020-02-19 15:46:42 -0800

    Added Apollo wrapper to App.test to make initial test pass

--- ui-react/src/App.test.js
+++ ui-react/src/App.test.js
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_URI
+});
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
