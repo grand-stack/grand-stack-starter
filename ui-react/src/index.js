a4e8702c7abc03137cecfc59522d9835ef6201a4
iamflowz <dakota.lewallen17@gmail.com>
2020-04-07 11:35:31 -0400

    Added React router

--- ui-react/src/index.js
+++ ui-react/src/index.js
@@ -5,15 +5,18 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URI
 });
 
 const Main = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
 
 ReactDOM.render(<Main />, document.getElementById("root"));
