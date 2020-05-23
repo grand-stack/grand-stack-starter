a914058aaea61e590e90047b1c5e54c0d08d9eac
johnymontana <lyonwj@gmail.com>
2020-05-23 15:57:15 -0600

    format

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -9,7 +9,7 @@ const neo4j = require('neo4j-driver')
 // Be sure to run `npm run build`
 const { typeDefs } = require('./graphql-schema')
 
-const driver =    neo4j.driver(
+const driver =     neo4j.driver(
   process.env.NEO4J_URI || 'bolt://localhost:7687',
   neo4j.auth.basic(
     process.env.NEO4J_USER || 'neo4j',
