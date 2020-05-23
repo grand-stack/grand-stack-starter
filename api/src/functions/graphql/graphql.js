fc7c31ba38cf3ca9846319a867c01077834608ba
johnymontana <lyonwj@gmail.com>
2020-05-23 14:04:35 -0600

    updates for serverless GraphQL deployment

--- api/src/functions/graphql/graphql.js
+++ api/src/functions/graphql/graphql.js
@@ -1,8 +1,13 @@
+// This module can be used to serve the GraphQL endpoint
+// as a lambda function
+
 const { ApolloServer } = require("apollo-server-lambda");
 const { makeAugmentedSchema } = require("neo4j-graphql-js");
-const neo4j  = require("neo4j-driver");
+const neo4j = require("neo4j-driver");
 
-import { typeDefs } from "../graphql-schema";
+// This module is copied during the build step
+// Be sure to run `npm run build`
+const { typeDefs } = require("./graphql-schema");
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI || "bolt://localhost:7687",
@@ -18,8 +23,6 @@ const driver = neo4j.driver(
 const server = new ApolloServer({
   schema: makeAugmentedSchema({ typeDefs }),
   context: { driver, neo4jDatabase: process.env.NEO4J_DATABASE },
-  playground: true,
-  introspection: true
 });
 
 exports.handler = server.createHandler();
